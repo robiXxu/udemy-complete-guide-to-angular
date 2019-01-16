8f3ffea04472206155ca9ff9401165af38e8c5a4
Schiriac Robert <schiriac.robert@gmail.com>
2019-01-16 17:38:51 +0200

    second: added ingredients mock, structured shopping list

--- second/src/app/components/shopping-list/shopping-list.component.ts
+++ second/src/app/components/shopping-list/shopping-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from 'src/app/models/ingredient.model';
+import * as ingredientData from 'src/mock/ingredients.json';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Array<Ingredient> = ingredientData.map(i => new Ingredient({...i}));
   constructor() { }
 
   ngOnInit() {
